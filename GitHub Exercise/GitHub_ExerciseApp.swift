0a6efd88644b5266b111f39f4010a848af71d6c2
Athanasios Siasios <sakizz@icloud.com>
2023-04-23 17:36:25 +0300

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Athanasios Siasios on 23/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
